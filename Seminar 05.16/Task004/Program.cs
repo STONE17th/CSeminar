e0763d7c7295acb9b33cf9dd82e1abb90493ff0e
STONE17th <17th@inbox.ru>
2022-05-26 13:59:35 +0300

    Добавлен README файл 'Задания'

--- Seminar 05.16/Task004/Program.cs
+++ Seminar 05.16/Task004/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Найти сумму чисел от 1 до А");
+﻿Console.WriteLine("Найти произведение чисел от 1 до А");
 Console.Write("Введите число: ");
 int NumberA =  int.Parse(Console.ReadLine() ?? "0");
 
@@ -6,4 +6,4 @@ int Num = 1;
 for (int i = 1; i <= NumberA; i++)
     Num *= i;
 
-Console.WriteLine($"Сумма чисел от 1 до {NumberA} равна {Num}");
+Console.WriteLine($"Произведение чисел от 1 до {NumberA} равна {Num}");
