c518862e743c1ea449c7bbb5902db70aee6d3f08
STONE17th <17th@inbox.ru>
2022-05-30 11:57:01 +0300

    Добавлен 6ой семинар

---
+++ Seminar 05.30/Task001/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.WriteLine("В матрице чисел найти сумму элементов главной диагонали");
+Console.Write("Введите размер квадратной матрицы: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[size,size];
+int summa = 0;
+var rnd = new Random();
+
+for (int i=0; i<size; i++)
+{
+    for (int j=0; j<size; j++)
+        matrix[i,j] = rnd.Next(0,10);
+    summa += matrix[i,i];
+}
+
+Console.WriteLine($"Квадратная матрица {size}х{size}");
+for (int i=0; i<size; i++)
+{
+    for (int j=0; j<size; j++)
+        Console.Write($"{matrix[i,j]}\t");
+    Console.WriteLine();
+}
+
+Console.Write($"Сумма элементов главной диагонали: {summa}");
---
+++ Seminar 05.30/Task002/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.WriteLine("Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов");
+Console.Write("Введите количество строк: ");
+int sizeCol = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов: ");
+int sizeRow = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[sizeCol,sizeRow];
+double Summa = 0;
+var rnd = new Random();
+
+for (int i=0; i<sizeCol; i++)
+{
+    for (int j=0; j<sizeRow; j++)
+    {
+        matrix[i,j] = rnd.Next(0,100);
+        Console.Write($"{matrix[i,j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int i=0; i<sizeCol; i++)
+{
+    Summa=0;
+    for (int j=0; j<sizeRow; j++)
+        Summa += matrix[j,i];
+    Console.WriteLine($"Среднее арифметическое {i+1} столбца: {Math.Round(Summa/sizeCol, 2)} ");
+}
