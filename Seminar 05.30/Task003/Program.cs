6fd5c80efc9fdb2002056bdea7ad5bbf2d07238e
STONE17th <17th@inbox.ru>
2022-05-30 12:04:56 +0300

    Добавлен 6ой семинар

---
+++ Seminar 05.30/Task003/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Посчитать сумму элементов побочной диагонали матрицы");
+Console.Write("Введите размер квадратной матрицы: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] matrix = new int[size,size];
+int summa = 0;
+var rnd = new Random();
+
+for (int i=0; i<size; i++)
+{
+    for (int j=0; j<size; j++)
+        matrix[i,j] = rnd.Next(0,6);
+    summa += matrix[i,size-i-1];
+}
+
+Console.WriteLine($"Квадратная матрица {size}х{size}");
+for (int i=0; i<size; i++)
+{
+    for (int j=0; j<size; j++)
+        Console.Write($"{matrix[i,j]} ");
+    Console.WriteLine();
+}
+Console.WriteLine($"Сумма побочной диагонали матрицы равна {summa}");
